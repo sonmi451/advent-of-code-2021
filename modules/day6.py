bc167e271b2798fda557737e6f6ec54292464148
Aisha <aishamclean451@gmail.com>
2021-12-09 23:08:56 +0000

    day 6 part 2

--- modules/day6.py
+++ modules/day6.py
@@ -1,14 +1,17 @@
-from read_datafile import read_datafile
+from read_datafile import read_sequence
+
+def get_lantern_fish_ages():
+    filepath = './data/day6.txt'
+    fish_ages = read_sequence(filepath)
+    return fish_ages
 
 def exponential_fish(lantern_fish, days):
 
     for day in range(1,days+1):
-        print(f'Day {day}')
+        # print(f'Day {day}')
 
         lantern_fish = lantern_fish.copy()
         num_starting_fish = len(lantern_fish)
-
-        print(f'> Before Countdown: {lantern_fish}')
 
         for fish in range(0, num_starting_fish):
             # count fish that reproduce
@@ -21,7 +24,13 @@ def exponential_fish(lantern_fish, days):
             else:
                 lantern_fish[fish] = (lantern_fish[fish] - 1) % 7
 
-        print(f'Day {day} fish: {lantern_fish} ')
+        print(f'Day {day} fish: {len(lantern_fish)} ')
 
     total_fish = len(lantern_fish)
     return total_fish
+
+days = 256
+starting_lantern_fish = get_lantern_fish_ages()
+total_fish = exponential_fish(starting_lantern_fish, days)
+
+print(total_fish)
